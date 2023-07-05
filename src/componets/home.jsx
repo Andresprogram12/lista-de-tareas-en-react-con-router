c58e13a9b9f882433d82390c9f348f7609363e00
Andresprogram12 <ibailandxd@gmail.com>
2023-07-04 21:32:27 -0600

    first commit

---
+++ src/componets/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la Aplicación</h1>
+      <p>¡Gracias por usar nuestra aplicación!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/componets/sobrenosotros.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SobreNosotros = () => {
+  return (
+    <div>
+      <h1>Sobre Nosotros</h1>
+      <p>Esta aplicación es una herramienta para administrar tus tareas de manera eficiente.</p>
+      <p>Funcionalidades:</p>
+      <ul>
+        <li>Crear tareas</li>
+        <li>Marcar tareas como completadas</li>
+        <li>Eliminar tareas</li>
+      </ul>
+      <p>Tecnologías utilizadas:</p>
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        {/* Agrega otras tecnologías utilizadas en tu aplicación */}
+      </ul>
+    </div>
+  );
+};
+
+export default SobreNosotros;
